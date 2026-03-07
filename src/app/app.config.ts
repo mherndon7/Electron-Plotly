2732963d8cac08cf09e955edec6e9746d0a1d658
Mitchell Herndon <mherndon2010@gmail.com>
2026-03-07 09:49:55 -0600

    Added 3D plot with angular-plotly

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { PlotlyModule } from 'angular-plotly.js';
+// @ts-ignore
+import Plotly from 'plotly.js-dist-min';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    importProvidersFrom(PlotlyModule.forRoot(Plotly)),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlotlyModule } from 'angular-plotly.js';
+import * as PlotlyJS from 'plotly.js-dist-min';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PlotlyModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('electron-app');
+  readonly graph = {
+    data: [
+      {
+        x: [1, 2, 3, 4, 5],
+        y: [10, 8, 12, 10, 14],
+        z: [5, 6, 7, 8, 9],
+        mode: 'markers',
+        marker: {
+          size: 12,
+          color: 'rgb(127, 127, 127)',
+          opacity: 0.8,
+        },
+        type: 'scatter3d', // Key property for 3D scatter plot
+      },
+    ],
+    layout: {
+      autosize: true,
+      title: '3D Scatter Plot Example',
+      height: 300,
+      width: 400,
+      scene: {
+        // Define 3D scene layout
+        xaxis: { title: 'X Value' },
+        yaxis: { title: 'Y Value' },
+        zaxis: { title: 'Z Value' },
+      },
+      margin: { l: 0, r: 0, b: 0, t: 30 },
+    },
+  };
 }
